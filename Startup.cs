71e0fb371cb192678cdc1ec6eaf2b955c14dd6c3
Michael Washington <webmaster@adefwebserver.com>
2020-09-12 07:14:23 -0700

    This is where you wire up to events to detect when a user logs in

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System.Security.Claims;
 
 namespace BlazorSimpleSurvey
 {
@@ -29,12 +32,75 @@ namespace BlazorSimpleSurvey
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
-                .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
-
             services.AddDbContext<SimpleSurveyContext>(options =>
             options.UseSqlServer(
                 Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
+                .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
+
+            // This is where you wire up to events to detect when a user logs in
+            services.Configure<OpenIdConnectOptions>(AzureADB2CDefaults.OpenIdScheme, options =>
+            {
+                options.Events = new OpenIdConnectEvents
+                {
+                    OnRedirectToIdentityProvider = async ctxt =>
+                    {
+                        // Invoked before redirecting to the identity provider to authenticate. 
+                        // This can be used to set ProtocolMessage.State
+                        // that will be persisted through the authentication process. 
+                        // The ProtocolMessage can also be used to add or customize
+                        // parameters sent to the identity provider.
+                        await Task.Yield();
+                    },
+                    OnAuthenticationFailed = async ctxt =>
+                    {
+                        // They tried to log in but it failed
+                        await Task.Yield();
+                    },
+                    OnTicketReceived = async ctxt =>
+                    {
+                        if (ctxt.Principal.Identity is ClaimsIdentity identity)
+                        {
+                            var colClaims = ctxt.Principal.Claims.ToList();
+                            var identityprovider = colClaims.FirstOrDefault(
+                                c => c.Type == "http://schemas.microsoft.com/identity/claims/identityprovider")?.Value;
+
+                            var idp_access_token = colClaims.FirstOrDefault(
+                                c => c.Type == "idp_access_token")?.Value;
+
+                            // Google login
+                            if (identityprovider.ToLower().Contains("google"))
+                            {
+
+                            }
+
+                            // Microsoft account login
+                            if (identityprovider.ToLower().Contains("live"))
+                            {
+
+                            }
+
+                            // Twitter login
+                            if (identityprovider.ToLower().Contains("twitter"))
+                            {
+
+                            }
+
+                            // Azure Active Directory login
+                            // But this will only work if Azure B2C Custom Policy is configured
+                            // to pass the idp_access_token
+                            // See \!AzureB2CConfig\TrustFrameworkExtensions.xml
+                            // for an example that does that
+                            if (idp_access_token != null)
+                            {
+
+                            }
+                        }
+                        await Task.Yield();
+                    },
+                };
+            });
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
